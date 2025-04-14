b81b60d72d919595b2d6ddd6e3819a0f4af1e32b
VARSHITHREDDY2225 <129868932+VARSHITHREDDY2225@users.noreply.github.com>
2025-04-14 15:23:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let cartCount = 0;
+let cartItems = [];
 
-function addToCart() {
+function addToCart(productName) {
   cartCount++;
+  cartItems.push(productName);
   document.getElementById('cart-count').textContent = cartCount;
+  alert(`${productName} has been added to your cart!`);
 }
